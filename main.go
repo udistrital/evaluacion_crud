3b455afac9d99ae8858badffba69ef15f4ffb694
Andrea <anlramirezs11@gmail.com>
2019-11-20 13:49:41 -0500

    Se añaden los CORS al CRUD. Issue #1

--- main.go
+++ main.go
@@ -6,14 +6,34 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/lib/pq"
+	"github.com/astaxie/beego/plugins/cors"
+	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
+	"github.com/udistrital/utils_oas/customerror"
+
 )
 
 func main() {
+	orm.Debug = true
+
 	orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("sqlconn"))
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders: []string{"Origin", "x-requested-with",
+		  "content-type",
+		  "accept",
+		  "origin",
+		  "authorization",
+		  "x-csrftoken"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	  }))
+	beego.ErrorController(&customerror.CustomErrorController{})
+	apistatus.Init()
 	beego.Run()
 }
 
