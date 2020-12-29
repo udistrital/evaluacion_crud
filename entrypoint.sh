6b97e4b4250fcdd6a62d09c6c21c0cae3e97cccb
Diego <ddguerran@correo.udistrital.edu.co>
2020-12-29 16:31:59 -0500

    devops: cambios en el parameter store

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -u
 set -o pipefail
 
 if [ -n "${PARAMETER_STORE:-}" ]; then
-  export EVALUACIONES_CRUD__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/novedades_crud/db/username --output text --query Parameter.Value)"
-  export EVALUACIONES_CRUD__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/novedades_crud/db/password --output text --query Parameter.Value)"
+  export EVALUACIONES_CRUD__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/evaluacion_crud/db/username --output text --query Parameter.Value)"
+  export EVALUACIONES_CRUD__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/evaluacion_crud/db/password --output text --query Parameter.Value)"
 fi
 
 exec ./main "$@"
